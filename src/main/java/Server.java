fe9bd27b3a4613e2fe1f681a5fce4b56d59f4b19
Pavel Vnuchkov <pavelvnuckov.com>
2024-11-29 12:29:05 +0300

    Initial commit

---
+++ src/main/java/Server.java
@@ -0,0 +1,100 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html",
+            "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+    public void start(int port) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            ExecutorService executorService = Executors.newFixedThreadPool(2);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                executorService.execute(() -> { processingMessage(socket);});
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void processingMessage(Socket socket) {
+
+        System.out.println("Обработчик");
+        try (
+        final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+        ) {
+
+            // read only request line for simplicity
+            // must be in form GET /path HTTP/1.1
+            final String requestLine = in.readLine();
+            if (requestLine == null) {
+                return;
+            }
+            final String[] parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                // just close socket
+                return;
+            }
+
+            final String path = parts[1];
+            if (!validPaths.contains(path)) {
+                out.write((
+                        "HTTP/1.1 404 Not Found\r\n" +
+                                "Content-Length: 0\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.flush();
+                return;
+            }
+
+            final var filePath = Path.of(".", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
+
+            // special case for classic
+            if (path.equals("/classic.html")) {
+                final var template = Files.readString(filePath);
+                final var content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+                return;
+            }
+
+            final var length = Files.size(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            out.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
